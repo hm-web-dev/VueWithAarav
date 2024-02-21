4a57f57bbe31c5af359d3728756ba3180064f7b2
Ms Feng <irene_feng@horacemann.org>
2024-02-21 09:49:39 -0500

    options api for app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
-<script setup>
+<script>
 import Header from './components/Header.vue'
 import News from './components/pages/News.vue' // TODO: remove
 import Footer from './components/Footer.vue'
+
+export default {
+  components: {
+    Header,
+    News,
+    Footer
+  }
+}
 </script>
 
 <template>
