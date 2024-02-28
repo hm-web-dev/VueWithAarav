a3b8f383e4252b76b7c3a14986b838ec05f27168
Ms Feng <irene_feng@horacemann.org>
2024-02-28 13:25:48 -0500

    added routing, SPA

--- src/App.vue
+++ src/App.vue
@@ -5,28 +5,23 @@ import Footer from './components/Footer.vue'
 import AboutUs from './components/pages/AboutUs.vue'
 import Matches from './components/pages/Matches.vue'
 // hi y'all, going to number the changes (there are 4) so it's clear to you. 
-
 // 1. create a routes object with all your navbar routes
 // see https://vuejs.org/guide/scaling-up/routing.html#simple-routing-from-scratch
 const routes = {
-  '/': null, 
-  '/news': News, 
-  '/about': AboutUs, 
-  '/matches': Matches,
+  '/': null,
+  '/news': News,
+  '/about': AboutUs,
 }
 export default {
   data() {
-    // 2. change the currentPath to be the value of the routes variable (instead of just a string)
+    // 2. change the currentPath to be the value of the routes variable (instead of just a string
     return {
       currentPath: routes['/']
     }
   },
   mounted() {
     window.addEventListener('hashchange', () => {
-      // 3. remove the hash so we just get the route, and make it the currentPath 
-      const removeHash = window.location.hash.slice(1);
-      console.log(removeHash)
-      this.currentPath = routes[removeHash || '/'];
+      this.currentPath = routes[window.location.hash.slice(1)];
     });
   },
   components: {
@@ -39,10 +34,9 @@ export default {
 
 <template>
   <Header />
-   <!-- 4. change this to be a dynamic component, and add :is="currentPath" as a directive -->
-   <component :is="currentPath" />
+  <!-- 4. change this to be a dynamic component, and add :is="currentPath" as a directive -->
+  <component :is="currentPath" />
   <Footer />
 </template>
 
-<style>
-</style>
+<style></style>
